d753b2c0e4c0368fb71becc969831953404eebf3
Afroz <afroz9045@gmail.com>
2022-05-23 12:32:09 +0530

    Basic Calculator Program Created

---
+++ Calc/ArithmeticFunctions/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace ArithmeticFunctions
+{
+    public class Class1
+    {
+        public static int Sum(int num1,int  num2)
+        {
+            return num1 + num2;
+        }
+        public static int Difference(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Product(int num1,int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Division(int num1,int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1,int num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,44 @@
+﻿//// See https://aka.ms/new-console-template for more information
+using ArithmeticFunctions;
+
+Console.WriteLine("Enter first number :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Enter operator :");
+string opr = Console.ReadLine();
+
+Console.WriteLine("Enter second number :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+switch (opr)
+{
+    case "+":
+        var result = Class1.Sum(num1,num2);
+        Console.WriteLine(result);
+        break;
+    case "-":
+        result = Class1.Difference(num1, num2);
+        Console.WriteLine(result);
+        break;
+    case "*":
+        result = Class1.Product(num1, num2);
+        Console.WriteLine(result);
+        break;
+    case "/":
+        result = Class1.Division(num1, num2);
+        Console.WriteLine(result);
+        break;
+    case "%":
+        result = Class1.Modulus(num1, num2);
+        Console.WriteLine(result);
+        break;
+    default:
+        Console.WriteLine("Invalid Operator");
+        break;
+}
+
+
