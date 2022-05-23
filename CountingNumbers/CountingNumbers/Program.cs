10a6f6ac1661c4d45133f0350c3c4371a5208340
Afroz <afroz9045@gmail.com>
2022-05-23 17:39:48 +0530

    Counting program with even number finder added

---
+++ CountingNumbers/CountingNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a number to get counting till it");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n");
+
+for(int i = 1; i <= number; i++)
+{
+    
+    if (i % 2 == 0)
+    {
+        
+        Console.WriteLine($"{i} is Even Number");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
