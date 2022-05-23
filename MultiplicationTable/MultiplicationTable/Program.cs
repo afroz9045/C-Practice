f42aae5b384fcb6499c58c23a951a5377a5ef5ee
Afroz <afroz9045@gmail.com>
2022-05-23 12:43:53 +0530

    Multiplication table program added

---
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a Number to get the multiplication table of it.");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num}*{i}={num * i}");
+}
