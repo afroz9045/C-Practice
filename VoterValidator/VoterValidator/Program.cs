e4924dac436dc191bfc5acca1f5aea44cd59188b
Afroz <afroz9045@gmail.com>
2022-05-24 18:06:56 +0530

    voter validator program added

---
+++ VoterValidator/VoterValidator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your nationality");
+string nationality = Console.ReadLine();
+
+if(age>=18 && (nationality == "india" || nationality == "indian"))
+{
+    Console.WriteLine("You Are eligible to vote");
+}
+else
+{
+    Console.WriteLine("Not eligible to vote");
+}
