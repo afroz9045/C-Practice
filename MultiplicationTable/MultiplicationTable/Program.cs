a1213003c253379e4015ec1c99e6c03d866802ee
Afroz <afroz9045@gmail.com>
2022-06-01 13:59:17 +0530

    refactored

--- MultiplicationTable/MultiplicationTable/Program.cs
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Enter a Number to get the multiplication table of it.");
+Console.WriteLine("Enter a number to get the multiplication table of it:");
 int num = Convert.ToInt32(Console.ReadLine());
 
 for(int i = 1; i <= 10; i++)
