6f0fa4a0f4df6148535a92c99d52baec73efed31
Afroz <afroz9045@gmail.com>
2022-06-02 17:01:50 +0530

    Temperature conversion added

---
+++ TemperatureUnitsConversion/TemperatureUnitConversion/TemperatureConversion.cs
@@ -0,0 +1,26 @@
+﻿namespace TemperatureUnitConversion
+{
+    public class TemperatureConversion
+    {
+
+        static string result;
+        public static string TemperatureConvert(float temperature,int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                   result = Convert.ToString((temperature*9)/5+32);
+                    result += "F";
+                    break;
+                case 2:
+                    result = Convert.ToString((temperature-32)*5/9);
+                    result += "C";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+            return TemperatureConversion.result;
+        }
+    }
+}
