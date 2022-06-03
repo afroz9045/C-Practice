859a50f9ca30ca0e02379c84e1fc86bfed4aec0c
Afroz <afroz9045@gmail.com>
2022-06-03 15:56:08 +0530

    Factorial Program Added

---
+++ FactorialFinder/FactorialFinder/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactorialLogic;
+
+Console.WriteLine("Enter a number to get factorial of it: ");
+int factorialNumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Factorial of {factorialNumber} is: {Factorial.FactorialCalculation(factorialNumber)}");
---
+++ FactorialFinder/FactorialLogic/Factorial.cs
@@ -0,0 +1,16 @@
+﻿namespace FactorialLogic
+{
+    public class Factorial
+    {
+        public static int FactorialCalculation(int number)
+        {
+            int factorialResult = 1;
+            while(number>0)
+            {
+                factorialResult *= number;
+                number--;
+            }
+            return factorialResult;
+        }
+    }
+}
