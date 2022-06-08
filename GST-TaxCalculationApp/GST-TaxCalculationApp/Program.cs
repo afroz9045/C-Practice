7e58d8c3d5ebd5a8ed9eadc18d673d8bb3b5ddea
Afroz <afroz9045@gmail.com>
2022-06-08 21:28:14 +0530

    GST calculation program added

---
+++ GST-TaxCalculationApp/GST-TaxCalculationApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GstCalculationLogic;
+
+Console.WriteLine("Enter product price:");
+double productPrice = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Press '1' for state level business");
+Console.WriteLine("Press '2' for outside state business");
+
+int selection = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+var totalPrice = productPrice + GstCalculation.TaxCalculation((float)productPrice, selection);
+Console.WriteLine($"Total price including GST is: {totalPrice}");
---
+++ GST-TaxCalculationApp/GstCalculation/GstCalculation.cs
@@ -0,0 +1,31 @@
+﻿namespace GstCalculationLogic
+{
+    public class GstCalculation
+    {
+        static float result;
+        public static float TaxCalculation(float productPrice, int selection)
+        {
+            int cgst = 9;
+            int sgst = 9;
+            int igst = 18;
+            try
+            {
+                switch (selection)
+                {
+
+                    case 1:
+                        result = (productPrice / 100)*(cgst + sgst);
+                        break;
+                    case 2:
+                        result = (productPrice / 100)* (igst);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+            return result;
+        }
+    }
+}
