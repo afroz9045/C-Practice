faef2f4d386634188ba5cb93d0672d9e1508c4f4
Afroz <afroz9045@gmail.com>
2022-06-15 20:31:58 +0530

    Emi calculator

---
+++ EmiCalculator/EmiCalculation/Emi.cs
@@ -0,0 +1,15 @@
+﻿namespace EmiCalculation
+{
+    public class Emi
+    {
+        public static string EmiCalculate(long productPrice,long downPayment,int tenure,int interestPercentage)
+        {
+            long loanAmount = productPrice - downPayment;
+            long interestAmount = (loanAmount / 100) * interestPercentage;
+            long emiAmount = loanAmount + interestAmount;
+            long monthlyEmi = emiAmount / tenure;
+            return $"Total EMI amount is {emiAmount}, monthly instalment is {monthlyEmi}";
+
+        }
+    }
+}
---
+++ EmiCalculator/EmiCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmiCalculation;
+Console.WriteLine("Enter product price:");
+long ProductPrice = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter down payment:");
+long DownPayment = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter loan tenure:");
+int Tenure = Convert.ToInt32(Console.ReadLine());
+
+int InterestPercentage = 8;
+
+Console.WriteLine(Emi.EmiCalculate(ProductPrice,DownPayment,Tenure,InterestPercentage));
+
