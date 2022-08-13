0669d43072ebafa0c076983c8c37ed62c2785812
Afroz <afroz9045@gmail.com>
2022-08-13 14:30:33 +0530

    Add pagination on list for practice

---
+++ pagging pracice/Pagging/Pagging/Program.cs
@@ -0,0 +1,8 @@
+﻿using Pagging.Infrastructure.Data;
+using Pagging.Infrastructure;
+
+var empData = InMemoryData.GetEmployees(3, 2);
+foreach (var employee in empData)
+{
+    Console.WriteLine($"{employee.EmpId} {employee.EmpName}");
+}
