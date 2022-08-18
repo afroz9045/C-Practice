60470cf60a03140672b18b876fb164c8a989db40
Afroz <afroz9045@gmail.com>
2022-08-18 17:48:51 +0530

    add dapper for stored procedure and view

---
+++ DapperPlayGround/DapperPlayground/DapperPlayGround.Infrastructure/Models/VMProductDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DapperPlayGround.Infrastructure.Models
+{
+    public partial class VMProductDetail
+    {
+        public string Name { get; set; } = null!;
+        public string ProductNumber { get; set; } = null!;
+        public int ProductId { get; set; }
+    }
+}
